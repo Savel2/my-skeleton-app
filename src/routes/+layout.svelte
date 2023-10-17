ac61d3c41006ba70872ff269d24a559d80f96367
Savel2 <saveliy2105@gmail.com>
2023-10-17 21:54:40 +0100

    Added shell, Appbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,10 +6,29 @@
 	import { storePopup } from '@skeletonlabs/skeleton';
 	import { AppShell } from '@skeletonlabs/skeleton';
 	import { AppBar } from '@skeletonlabs/skeleton';
-	import { Avatar } from '@skeletonlabs/skeleton'
+	
 
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 	
 </script>
 
-<slot />
+
+
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+		<svelte:fragment slot="lead">(icon)</svelte:fragment>
+		
+		<svelte:fragment slot="trail">(actions)</svelte:fragment>
+		</AppBar></svelte:fragment>
+	<!-- (sidebarLeft) -->
+	<!-- (sidebarRight) -->
+	<!-- (pageHeader) -->
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
+	<!-- (footer) -->
+</AppShell>
+
+
