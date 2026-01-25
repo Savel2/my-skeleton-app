787c0e3ef6a4ba34c97eb8feaf501cf17ee0d130
Savel2 <saveliy2105@gmail.com>
2026-01-25 19:29:00 +0000

    Made Chat changes to tailwind.ts

---
+++ tailwind.config.cjs
@@ -0,0 +1,29 @@
+const { join } = require('path')
+const forms = require('@tailwindcss/forms')
+const typography = require('@tailwindcss/typography')
+const { skeleton } = require('@skeletonlabs/tw-plugin')
+const { myCustomTheme } = require('./my-custom-theme')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './src/**/*.{html,js,svelte,ts}',
+    join(
+      require.resolve('@skeletonlabs/skeleton'),
+      '../**/*.{html,js,svelte,ts}'
+    ),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    forms,
+    typography,
+    skeleton({
+      themes: {
+        custom: [myCustomTheme],
+      },
+    }),
+  ],
+}
